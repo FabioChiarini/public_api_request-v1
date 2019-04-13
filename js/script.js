8fe76e6579e006b28b43cc76d6aeaffd2e31088a
Fabio Chiarini <chiarrss@gmail.com>
2019-04-13 17:25:48 +0200

    First implementation of divs and picture of random employee

--- js/script.js
+++ js/script.js
@@ -2,4 +2,28 @@
  * Project 5 - Public API Request
  */
 
- 
+
+
+
+ $.ajax({
+  url: 'https://randomuser.me/api/?results=12',
+  dataType: 'json',
+  success: function(data) {
+    data.results.forEach(function(randomUser) {
+      let employee = '<div class="card"><div class="card-img-container">';
+      employee += '<img class="card-img" src="' + randomUser.picture.large + '" alt="profile picture"></div></div>';
+      $('.gallery').append(employee);
+      employee = '';
+      console.log(randomUser.picture.large);
+
+    });
+    //console.log(data.results);
+  }
+});
+
+/*
+$.each( obj, function( key, value ) {
+  alert( key + ": " + value );
+});
+Once again, this
+*/
