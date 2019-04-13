7e3174560e385687eb0e2485db7cbd7734ec0929
Fabio Chiarini <chiarrss@gmail.com>
2019-04-13 19:07:01 +0200

    Implemented show modal employee with closing

--- js/script.js
+++ js/script.js
@@ -11,8 +11,10 @@
   success: function(data) {
     /*loop through each random user obtained from the request
     and format the html that will contain the necessary data */
+    /*username was added to the element in order to univocally distinguish the employees
+    in the modal part of the project*/
     data.results.forEach(function(randomUser) {
-      let employee = '<div class="card"><div class="card-img-container">';
+      let employee = '<div class="card" id="' + randomUser.login.username + '"><div class="card-img-container">';
       employee += '<img class="card-img" src="' + randomUser.picture.large + '" alt="profile picture"></div>';
       employee += '<div class="card-info-container">'
       employee += '<h3 id="name" class="card-name cap">' + randomUser.name.first + ' ' + randomUser.name.last + '</h3>';
@@ -23,12 +25,38 @@
       console.log(randomUser);
     });
 
+
+
+
     /*add a click event on every div of the page to display the modal of the chosen employee*/
     $('.card').each(function () {
       this.addEventListener('click', function (){
-        console.log(this);
+        //store the username of the selected employee to display the modal
+        var selectedEmployee = this.id;
+        /*loop trhough each employee and, when the one chosen has been found
+        display his modal stats*/
+        data.results.forEach(function(randomUser) {
+          if (randomUser.login.username === selectedEmployee) {
+            //set the html page format to display the chosen employee
+            let modalEmployee = '<div class="modal-container"><div class="modal">';
+            modalEmployee += '<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>';
+            modalEmployee += '<div class="modal-info-container">';
+            modalEmployee += '<img class="modal-img" src="'+ randomUser.picture.large +'" alt="profile picture">';
+            modalEmployee += '<h3 id="name" class="modal-name cap">' + randomUser.name.first + ' ' + randomUser.name.last + '</h3>';
+            modalEmployee += '<p class="modal-text">' + randomUser.email + '</p>';
+            modalEmployee += '<p class="modal-text cap">' + randomUser.location.city + '</p><hr>';
+            modalEmployee += '<p class="modal-text">' + randomUser.cell + '</p>';
+            modalEmployee += '<p class="modal-text">' + randomUser.location.street + ', ' + randomUser.location.city + ', ' + randomUser.location.state + ', ' + randomUser.location.postcode + '</p>';
+            modalEmployee += '<p class="modal-text">Birthday: ' + randomUser.dob.date.substring(0,10) + '</p></div></div></div>';
+            $('.gallery').append(modalEmployee);
+            modalEmployee = '';
+            //activate the closing button on the modal element
+            $('#modal-close-btn').click(function() {
+              $('.modal-container').remove();
+            });
+          }
+        });
       });
-
     });
   }
 });
